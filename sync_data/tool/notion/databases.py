dcf76a20b6e19181c90e741f4dacbacd2a48588d
Jason_Smith <qili00001@gmail.com>
2022-03-07 10:22:58 +0800

    add music database

--- sync_data/tool/notion/databases.py
+++ sync_data/tool/notion/databases.py
@@ -8,7 +8,7 @@ import re
 
 import requests
 
-from sync_data.tool.douban.data.enum_data import MediaInfo, MediaStatus
+from sync_data.tool.douban.data.enum_data import MediaInfo, MediaStatus, MediaType
 from sync_data.utils import log_detail
 from sync_data.tool.notion import base
 from sync_data.utils.http_utils import RequestUtils
@@ -102,58 +102,96 @@ def get_book_body(data_dict, database_id, media_status):
     return body
 
 
-def create_database(token, page_id):
+def create_database(token, page_id, media_type):
     """
     创建新的数据库
+    :param media_type: 媒体类型 book music tv movie
     :param token: notion->setting->integrations->develop your own integrations
     :param page_id: 浏览器打开notion，链接的尾部获取
     :return: databases_id，可以通过该id定位到数据库
     """
     # TODO 添加书 影 音 数据库
-    db_data = base.NotionBaseInfo(token=token)
-    log_detail.info("【RNU】创建数据库--初始化参数")
-    create_db_data = {
-        "parent": {"type": "page_id", "page_id": f"{page_id}"},
-        "title": [{"type": "text", "text": {"content": "豆瓣书单库"}}],
-        "icon": {"type": "emoji", "emoji": "📚"},
-        "properties": {
-            "书名": {"title": {}},
-            "评分": {"number": {}},
-            "豆瓣链接": {"url": {}},
-            "作者": {"rich_text": {}},
-            "出版社": {"select": {}},
-            "ISBN": {"url": {}},
-            "封面": {"files": []},
-            "出版年份": {"select": {}},
-            "价格": {"number": {}},
-            "评分人数": {"number": {}},
-            "页数": {"number": {}},
-            "短评": {"rich_text": {}},
-            "类型": {"multi_select": {}},
-            "标记状态": {"select": {}},
-            "标记时间": {"date": {}},
-            "个人评分": {"select": {"options": [
-                {"name": "⭐", "color": "yellow"},
-                {"name": "⭐⭐", "color": "yellow"},
-                {"name": "⭐⭐⭐", "color": "yellow"},
-                {"name": "⭐⭐⭐⭐", "color": "yellow"},
-                {"name": "⭐⭐⭐⭐⭐", "color": "yellow"}]}},
-        }}
 
-    params = json.dumps(create_db_data)
-    # print(params)
-    log_detail.debug("【RNU】创建数据库--post请求")
-    # res = RequestUtils.post(url=db_data.get_db_url(), params=params, headers=db_data.get_headers())
-    db_res = RequestUtils()
-    res = db_res.post(url=db_data.get_db_url(), params=params, headers=db_data.get_headers())
-    if res.status_code == 200:
-        log_detail.debug(res.text)
-        database_id = eval(res.text.replace(":null", ":'null'").replace(":false", ":'false'"))["id"]
-        return database_id
+    if media_type == MediaType.BOOK.value:
+        create_db_data = {
+            "parent": {"type": "page_id", "page_id": f"{page_id}"},
+            "title": [{"type": "text", "text": {"content": "豆瓣书单库"}}],
+            "icon": {"type": "emoji", "emoji": "📚"},
+            "properties": {
+                "书名": {"title": {}},
+                "评分": {"number": {}},
+                "豆瓣链接": {"url": {}},
+                "作者": {"rich_text": {}},
+                "出版社": {"select": {}},
+                "ISBN": {"url": {}},
+                "封面": {"files": []},
+                "出版年份": {"select": {}},
+                "价格": {"number": {}},
+                "评分人数": {"number": {}},
+                "页数": {"number": {}},
+                "短评": {"rich_text": {}},
+                "类型": {"multi_select": {}},
+                "标记状态": {"select": {}},
+                "标记时间": {"date": {}},
+                "个人评分": {"select": {"options": [
+                    {"name": "⭐", "color": "yellow"},
+                    {"name": "⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐⭐⭐", "color": "yellow"}]}},
+            }}
+    elif media_type == MediaType.MUSIC.value:
+        create_db_data = {
+            "parent": {"type": "page_id", "page_id": f"{page_id}"},
+            "title": [{"type": "text", "text": {"content": "豆瓣音乐库"}}],
+            "icon": {"type": "emoji", "emoji": "🎵"},
+            "properties": {
+                "音乐": {"title": {}},
+                "表演者": {"rich_text": {}},
+                "封面": {"files": []},
+                "评分": {"number": {}},
+                "出版者": {"select": {}},
+                "发行时间": {"select": {}},
+                "ISRC": {"url": {}},
+                "豆瓣链接": {"url": {}},
+                "评分人数": {"number": {}},
+                "短评": {"rich_text": {}},
+                "类型": {"multi_select": {}},
+                "标记状态": {"select": {}},
+                "标记时间": {"date": {}},
+                "个人评分": {"select": {"options": [
+                    {"name": "⭐", "color": "yellow"},
+                    {"name": "⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐⭐", "color": "yellow"},
+                    {"name": "⭐⭐⭐⭐⭐", "color": "yellow"}]}},
+            }}
     else:
-        log_detail.warn("【RUN】创建数据库失败，请检查是否页面有授权给【集权】，再重新使用本程序")
-        input("请按Enter键结束！")
-        exit()
+        create_db_data = {}
+        log_detail.warn("【RUN】暂不支持其他数据库的创建")
+
+    try:
+        if create_db_data:
+            log_detail.info(f"【RNU】创建{media_type}数据库--初始化参数")
+            db_data = base.NotionBaseInfo(token=token)
+            params = json.dumps(create_db_data)
+            # print(params)
+            log_detail.debug(f"【RNU】创建{media_type}数据库--post请求")
+            # res = RequestUtils.post(url=db_data.get_db_url(), params=params, headers=db_data.get_headers())
+            db_res = RequestUtils()
+            res = db_res.post(url=db_data.get_db_url(), params=params, headers=db_data.get_headers())
+            if res.status_code == 200:
+                log_detail.debug(res.text)
+                database_id = eval(res.text.replace(":null", ":'null'").replace(":false", ":'false'"))["id"]
+                return database_id
+            else:
+                log_detail.warn("【RUN】创建数据库失败，请检查是否页面有授权给【集权】，再重新使用本程序")
+                input("请按Enter键结束！")
+                exit()
+        else:
+            log_detail.warn(f"【RUN】跳过创建{media_type}数据库")
+    except Exception as err:
+        log_detail.error(f"【RUN】创建数据库错误{err}")
 
 
 def update_database(data_dict, database_id, token, media_status):
@@ -178,7 +216,7 @@ def update_database(data_dict, database_id, token, media_status):
             log_detail.info(f"【RUN】导入《{data_dict[MediaInfo.TITLE.value]}》成功")
             return None
     except Exception as err:
-        log_detail.error(f"【RUN】{err}")
+        log_detail.error(f"【RUN】导入数据库错误：{err}")
         return None
 
 
