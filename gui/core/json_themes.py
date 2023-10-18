c60bd09fba81894eadab9080b09f2611186f0114
Qliangw <qili00001@gmail.com>
2023-10-18 16:15:16 +0800

    style: 调整主题配置的位置参数

--- gui/core/json_themes.py
+++ gui/core/json_themes.py
@@ -1,4 +1,3 @@
-# ///////////////////////////////////////////////////////////////
 #
 # BY: WANDERSON M.PIMENTA
 # PROJECT MADE WITH: Qt Designer and PySide6
@@ -12,35 +11,28 @@
 # commercially, I recommend reading them on the official website:
 # https://doc.qt.io/qtforpython/licenses.html
 #
-# ///////////////////////////////////////////////////////////////
 
 # IMPORT PACKAGES AND MODULES
-# ///////////////////////////////////////////////////////////////
 import json
 import os
 
 # IMPORT SETTINGS
-# ///////////////////////////////////////////////////////////////
 from gui.core.json_settings import Settings
 
 # APP THEMES
-# ///////////////////////////////////////////////////////////////
 class Themes(object):
     # LOAD SETTINGS
-    # ///////////////////////////////////////////////////////////////
     setup_settings = Settings()
     _settings = setup_settings.items
 
     # APP PATH
-    # ///////////////////////////////////////////////////////////////
-    json_file = f"gui/themes/{_settings['theme_name']}.json"
+    json_file = f"doc/themes/{_settings['theme_name']}.json"
     app_path = os.path.abspath(os.getcwd())
     settings_path = os.path.normpath(os.path.join(app_path, json_file))
     if not os.path.isfile(settings_path):
-        print(f"WARNING: \"gui/themes/{_settings['theme_name']}.json\" not found! check in the folder {settings_path}")
+        print(f"WARNING: \"doc/themes/{_settings['theme_name']}.json\" not found! check in the folder {settings_path}")
 
     # INIT SETTINGS
-    # ///////////////////////////////////////////////////////////////
     def __init__(self):
         super(Themes, self).__init__()
 
@@ -51,14 +43,12 @@ class Themes(object):
         self.deserialize()
 
     # SERIALIZE JSON
-    # ///////////////////////////////////////////////////////////////
     def serialize(self):
         # WRITE JSON FILE
         with open(self.settings_path, "w", encoding='utf-8') as write:
             json.dump(self.items, write, indent=4)
 
     # DESERIALIZE JSON
-    # ///////////////////////////////////////////////////////////////
     def deserialize(self):
         # READ JSON FILE
         with open(self.settings_path, "r", encoding='utf-8') as reader:
